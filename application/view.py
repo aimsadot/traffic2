7068c5cbe3834a7f59bc59126548ca68c9bcd0b0
Sadot Arefin <sarefin@apple.com>
2022-12-31 16:11:42 +0600

    data access init

--- application/view.py
+++ application/view.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 from flask_login import login_required
+
+from . import FLASH_ERROR, FLASH_SUCCESS
 
 view = Blueprint('view', __name__)
 
@@ -10,7 +12,16 @@ def home():
     return render_template("home.html")
 
 
-@view.route('/settings')
+@view.route('/settings', methods=['GET', 'POST'])
 @login_required
 def settings():
+    if request.method == 'POST':
+        mod = request.form.get('mod')
+        if 'file' in request.form:
+            print('Yes')
+        add_dic = {}
+        for item in request.form:
+            add_dic['item'] = request.form.get(item)
+
+        flash('mod is ' + request.form.get('mod'), category=FLASH_SUCCESS)
     return render_template("settings.html", areas=[])
